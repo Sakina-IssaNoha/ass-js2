5fff8f0ebfc05d47d633580dd369601f33c5ffa8
Sakina-IssaNoha <sakinnanoha@gmail.com>
2022-08-07 21:46:59 +0300

    changes

--- quiz2.js
+++ quiz2.js
@@ -1,10 +1,26 @@
-class KioskCalc {
-    constructor(fruitsPriceList){
-        this.fruitsPriceList = fruitsPriceList
+// class KioskCalc {
+//     constructor(fruitsPriceList){
+//         this.fruitsPriceList = fruitsPriceList
+//     }
+// }
+// let kiosk = new KioskCalc( {"orange": 30, "mango": 15, "avocado": 40 })
+// KioskCalc.prototype.getTotalCost = function(fruit,quantity){
+//     return `${quantity} ${fruit} for ksh ${quantity*this.fruitsPriceList.mango}`
+// }
+// console.log(kiosk.getTotalCost("mango",5))
+
+
+class KioskCalc{
+    constructor(fruit,quantity){
+        this.fruit=fruit;
+        this.quantity=quantity;
+        this.fruitsPriceList={"orange": 30, "mango": 15, "avocado": 40 };
+        
+        this.getTotalCost=function(){
+            return `${this.quantity} ${this.fruit} for KES ${this.fruitsPriceList[this.fruit]*this.quantity}`
+        }
     }
 }
-let kiosk = new KioskCalc( {"orange": 30, "mango": 15, "avocado": 40 })
-KioskCalc.prototype.getTotalCost = function(fruit,quantity){
-    return `${quantity} ${fruit} for ksh ${quantity*this.fruitsPriceList.mango}`
-}
-console.log(kiosk.getTotalCost("mango",5))
+
+let kiosk = new KioskCalc("avocado",7)
+console.log(kiosk.getTotalCost())
--- script1.js
+++ script1.js
@@ -1,10 +1,21 @@
-let fruits = {
-    Orange:30,
-    Watermelon:100,
-    Mango:20, 
-    CalculateFruitCost : function(fruit,quantity){
-        return ` ${quantity} ${fruit}  for KES ${this.Orange*quantity}`
+// let fruits = {
+//     Orange:30,
+//     Watermelon:100,
+//     Mango:20, 
+//     CalculateFruitCost : function(fruit,quantity){
+//         return ` ${quantity} ${fruit}  for KES ${this.Watermelon*quantity}`
 
+// }
+// }
+// console.log(fruits.CalculateFruitCost("Orange",2))
+
+
+let fruits={
+    listOfFruits: {"Watermelon":100,"Apple":30,"Grapes":250,"Orange":20},
+    calcFruitCost: function (fruit,quantity){
+        return `${quantity} ${fruit} for KES ${this.listOfFruits[fruit]*quantity}`
+    }
 }
-}
-console.log(fruits.CalculateFruitCost("Orange",2))
+console.log(fruits.calcFruitCost("Orange",2))
+
+
